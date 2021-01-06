8208119ca595bc1aa5c762e759918600b6143877
Decyferable <marisa.rivera.sarah@gmail.com>
2021-01-06 17:56:22 -0500

    removed minecraft, download will be hosted separately

--- net/minecraft/optifine/IFileDownloadListener.java
+++
@@ -1,6 +0,0 @@
-package net.minecraft.optifine;
-
-public interface IFileDownloadListener
-{
-    void fileDownloadFinished(String var1, byte[] var2, Throwable var3);
-}
--- net/minecraft/src/CustomItemProperties.java
+++
@@ -1,5 +0,0 @@
-package net.minecraft.src;
-
-public class CustomItemProperties
-{
-}
